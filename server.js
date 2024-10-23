3621b87c79cf6cf9ce7dc1d2e8f204abd88f21e3
Igor <igormartins.all@gmail.com>
2024-10-23 17:13:04 -0300

    creating routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const server = express()
 
+server.get('/', function(request, response) {
+  return response.send('Hi!')
+})
+
 server.listen(5000, function() {
   console.log('server is running!')
 })
