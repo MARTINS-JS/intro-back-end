3e324988d4d2adb2d24e93ea645344aa336d266f
Igor <igormartins.all@gmail.com>
2024-10-24 13:50:58 -0300

    bringing the website front-end to the server

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const nunjucks = require('nunjucks')
 const server = express()
 
 server.set('view engine', 'html')
+server.use(express.static('public'))
 
 nunjucks.configure('views', {
   express: server
 })
 
 server.get('/', function(request, response) {
-  return response.send("Hi, how's going?")
+  return response.render('index')
+})
+
+server.get('/portfolio', function(request, response) {
+  return response.render('portfolio')
 })
 
 server.listen(5000, function() {
