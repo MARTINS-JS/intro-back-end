11c0ae3e43ea2831bfea7fe7f6c9c1ed9431f6e0
Igor <igormartins.all@gmail.com>
2024-10-25 19:06:50 -0300

    updating about page

--- server.js
+++ server.js
@@ -9,11 +9,25 @@ server.set('view engine', 'njk')
 server.use(express.static('public'))
 
 nunjucks.configure('views', {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get('/', function(request, response) {
-  return response.render('about')
+
+  const pageDataAbout = {
+    avatar_url: 'https://avatars.githubusercontent.com/u/179001313?v=4',
+    name: 'Martins',
+    role: 'Estudante - Full Stack',
+    description: 'Determinado em aprender programação na <a target="_blank" href="https://www.rocketseat.com.br/">Rocketseat</a>',
+    links: [
+      { name: 'Instagram', url: '#' },
+      { name: 'GitHub', url: '#' },
+      { name: 'LinkedIn', url: '#' }
+    ]
+  }
+
+  return response.render('about', { data: pageDataAbout })
 })
 
 server.get('/portfolio', function(request, response) {
