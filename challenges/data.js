74e6fdef78f8bdf72e5cd9072ccd96a9fc2731fc
Igor <igormartins.all@gmail.com>
2024-10-26 09:31:48 -0300

    nunjucks files and dynamic data

---
+++ challenges/data.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 'python-linguagem-popular-com-berco-na-comedia-inglesa/',
+    technology: 'python',
+    title: 'Python: linguagem popular com berço na comédia inglesa',
+    author: 'Felipe Buzzi'
+  },
+  {
+    id: 'elixir-por-tras-da-linguagem-de-programacao-brasileira/',
+    technology: 'elixir',
+    title: 'Elixir: por trás da linguagem de programação brasileira',
+    author: 'Felipe Buzzi'
+  },
+  {
+    id: 'typescript-um-superset-de-javascript-que-adicionou-tipagem-e-novos-recursos-na-linguagem/',
+    technology: 'typescript',
+    title: 'TypeScript: por trás do superset de JavaScript',
+    author: 'Felipe Buzzi'
+  },
+  {
+    id: 'npm-pacotes-pacotes-e-pacotes/',
+    technology: 'npm',
+    title: 'npm: pacotes, pacotes e pacotes',
+    author: 'Felipe Buzzi'
+  }
+]
--- challenges/server.js
+++ challenges/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 
+const database = require('./data')
+
 const server = express()
 
 server.set('view engine', 'njk')
@@ -11,11 +13,38 @@ nunjucks.configure('views', {
 })
 
 server.get('/', function(request, response) {
-  return response.render('about')
+  const pageDataAbout = {
+    logo_url: '/rocketseat-symbol.svg',
+    name: 'Rocketseat',
+    role: 'A plataforma completa para você aprender programação do zero no seu ritmo, se tornar full stack e se especializar em diversas tecnologias sem sair de casa.',
+    technologies: [
+      'JavaScript',
+      'NodeJS',
+      'DevOps',
+      'ReactJS',
+      'React Native'
+    ],
+    links: [
+      {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/rocketseat/'
+      },
+      {
+        name: 'GitHub',
+        url: 'https://github.com/rocketseat'
+      },
+      {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/rocketseat'
+      }
+    ]
+  }
+
+  return response.render('about', { data: pageDataAbout })
 })
 
 server.get('/articles', function(request, response) {
-  return response.render('articles')
+  return response.render('articles', { cards: database })
 })
 
 server.use(function(request, response) {
